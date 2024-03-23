ab2692025ad68dc9b31f853b84e4a68d0b625d63
ssoyeeon <163149786+ssoyeeon@users.noreply.github.com>
2024-03-23 12:58:24 +0900

    auth_commited

---
+++ 24-03-23/Hello World.cs
@@ -0,0 +1,15 @@
+// 문제 : Hello World
+// 결과 :  / 속도: 60 / 메모리 : 5320
+// 제출시각 : 24-03-23  12:58:23
+using System;
+​
+namespace HELLOWORLD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
