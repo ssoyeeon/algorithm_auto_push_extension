6b731db06f3213b6fef16931c09be39f2838fb9d
ssoyeeon <163149786+ssoyeeon@users.noreply.github.com>
2024-03-26 16:34:09 +0900

    (auto_commited) 대소문자 바꿔서 출력하기

--- 24-03-26/대소문자 바꿔서 출력하기.cs
+++ 24-03-26/대소문자 바꿔서 출력하기.cs
@@ -1,6 +1,6 @@
 // 문제 : 대소문자 바꿔서 출력하기
 // 결과 :  / 속도: 0 / 메모리 : 0
-// 제출시각 : 24-03-26  14:36:54
+// 제출시각 : 24-03-26  16:34:6
 using System;
 ​
 public class Example
@@ -14,11 +14,11 @@ public class Example
         
         String result = "";
         
-
+        foreach (char c in s)
         {
-            if(char.IsLower(c))
+
             {
-                result += char.ToUpper(c);
+                result += char.ToUpper(c); // result = result + char.ToUpper(c)
             }
             else if (char.IsUpper(c))
             {
