730bd82a7dd8cc3ec67795059e62e631e419d1bf
ssoyeeon <163149786+ssoyeeon@users.noreply.github.com>
2024-03-26 18:11:26 +0900

    (auto_commited) 특수문자 출력하기

---
+++ 24-03-26/특수문자 출력하기.cs
@@ -0,0 +1,14 @@
+// 문제 : 특수문자 출력하기
+// 결과 :  / 속도: 0 / 메모리 : 0
+// 제출시각 : 24-03-26  18:11:23
+using System;
+​
+public class Example
+{
+    public static void Main()
+    {
+        string s = "!@#$%^&*(\\\'\"<>?:;"; //문자열 string에 변수 s 넣고 특수문자 넣음, ( 가 그거라 \\\로 해결한 것 같아 보이는데 아닌가염 아님 말고....ㅠㅠ
+        
+        Console.Write(s); //s 실행
+    }
+
