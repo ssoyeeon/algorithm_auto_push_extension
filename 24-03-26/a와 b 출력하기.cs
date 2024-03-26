d7593e0b54ccf15e15d9225fa0ba0b220dd5f8e9
ssoyeeon <163149786+ssoyeeon@users.noreply.github.com>
2024-03-26 16:34:07 +0900

    (auto_commited) a와 b 출력하기

---
+++ 24-03-26/a와 b 출력하기.cs
@@ -0,0 +1,21 @@
+// 문제 : a와 b 출력하기
+// 결과 :  / 속도: 0 / 메모리 : 0
+// 제출시각 : 24-03-26  16:34:3
+using System;
+​
+public class Example
+{
+    public static void Main()
+    {
+        String[] s;
+​
+        Console.Clear();
+
+​
+        int a = Int32.Parse(s[0]); //4
+        int b = Int32.Parse(s[1]); //5
+​
+        Console.WriteLine("a = {0}", a); // {0} = a
+        Console.WriteLine("b = {0}", b);
+    }
+}
