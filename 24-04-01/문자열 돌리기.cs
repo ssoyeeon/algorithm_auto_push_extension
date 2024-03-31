1001b25eedbf0031ea80c9bf3df67d469defbc41
ssoyeeon <163149786+ssoyeeon@users.noreply.github.com>
2024-04-01 08:57:56 +0900

    (auto_commited) 문자열 돌리기

---
+++ 24-04-01/문자열 돌리기.cs
@@ -0,0 +1,19 @@
+// 문제 : 문자열 돌리기
+// 결과 :  / 속도: 0 / 메모리 : 0
+// 제출시각 : 24-04-01  8:57:53
+using System;
+​
+public class Example
+{
+    public static void Main()
+    {
+        String s;
+​
+        Console.Clear();        //초기화
+        s = Console.ReadLine();     //한 줄의 입력을 받아서 문자열 변수 s에 저장
+        
+        foreach (char c in s)       // 문자열 s의 각 문자를 순회하면서 콘솔에 한 문자씩 출력
+
+​
+    }
+}
