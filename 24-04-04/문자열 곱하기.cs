8ae2b2f1e182c0c7127d79fce5031d89fee052ce
ssoyeeon <163149786+ssoyeeon@users.noreply.github.com>
2024-04-04 10:59:14 +0900

    (auto_commited) 문자열 곱하기

---
+++ 24-04-04/문자열 곱하기.cs
@@ -0,0 +1,16 @@
+// 문제 : 문자열 곱하기
+// 결과 :  / 속도: 0 / 메모리 : 0
+// 제출시각 : 24-04-04  10:59:10
+using System;
+​
+public class Solution {
+
+        string answer = "";     //문자열 출력
+        
+        for(int i=0; i<k; i++)      //int i를 K보다 작게 계속 더함
+        {
+            answer +=my_string;     //my_string에 for 값을 넣음
+        }
+        return answer;
+    }
+}
