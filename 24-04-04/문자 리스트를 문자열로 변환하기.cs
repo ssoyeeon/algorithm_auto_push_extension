52149413123ee44f1e2605958922d8d4a9bfb6e2
ssoyeeon <163149786+ssoyeeon@users.noreply.github.com>
2024-04-04 09:08:19 +0900

    (auto_commited) 문자 리스트를 문자열로 변환하기

---
+++ 24-04-04/문자 리스트를 문자열로 변환하기.cs
@@ -0,0 +1,14 @@
+// 문제 : 문자 리스트를 문자열로 변환하기
+// 결과 :  / 속도: 0 / 메모리 : 0
+// 제출시각 : 24-04-04  9:8:16
+using System;
+​
+public class Solution {
+
+        string answer = "";     // 결과 문자열 초기화
+        
+        answer = string.Concat(arr);        // 문자열 배열을 이어붙여서 결과 문자열에 저장
+        
+        return answer;
+    }
+}
