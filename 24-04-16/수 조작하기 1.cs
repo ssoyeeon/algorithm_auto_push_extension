ac25e78a9fda0f0d1eeb7c7d69b3317146057ad9
ssoyeon <ssuu2039@gmail.com>
2024-04-16 09:16:47 +0900

    수 조작하기 1

---
+++ 24-04-16/수 조작하기 1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Solution
+{
+    public int solution(int n, string control)
+    {
+        int answer = n;
+        for (int i = 0; i < control.Length; i++)
+        {
+            switch (control[i])
+            {
+                case 'w':
+                    answer += 1;
+                    break;
+                case 's':
+                    answer -= 1;
+                    break;
+                case 'd':
+                    answer += 10;
+                    break;
+                case 'a':
+                    answer -= 10;
+                    break;
+            }
+        }
+        return answer;
+    }
+}
